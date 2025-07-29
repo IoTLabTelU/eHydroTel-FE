1995205797b262dd797ca9ce51f043333baaf6f3
krisnaganesha1609 <igkganes@gmail.com>
2025-07-29 17:36:38 +0700

    feat: added button components

---
+++ lib/core/components/buttons.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hydro_iot/res/res.dart';
+
+Widget primaryButton({
+  required String text,
+  required VoidCallback onPressed,
+  required BuildContext context,
+  Color? color,
+  double? width,
+  double? height,
+}) {
+  return ElevatedButton(
+    onPressed: onPressed,
+    style: ElevatedButton.styleFrom(
+      backgroundColor: ColorValues.iotMainColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 5.w),
+    ),
+    child: Text(
+      text,
+      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+        color: ColorValues.whiteColor,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
+
+Widget secondaryButton({
+  required String text,
+  required VoidCallback onPressed,
+  required BuildContext context,
+}) {
+  return ElevatedButton(
+    onPressed: onPressed,
+    style: ElevatedButton.styleFrom(
+      backgroundColor: ColorValues.whiteColor,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+      padding: EdgeInsets.symmetric(vertical: 15.h, horizontal: 5.w),
+    ),
+    child: Text(
+      text,
+      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+        color: ColorValues.blackColor,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    ),
+  );
+}
