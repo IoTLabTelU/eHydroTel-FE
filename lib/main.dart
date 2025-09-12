414d19dd7dcb01a8f2f23c36e26a2d1560e72936
krisnaganesha1609 <igkganes@gmail.com>
2025-09-12 22:38:43 +0700

    chore: rive init

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,14 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:hydro_iot/core/app.dart';
+import 'package:rive/rive.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await RiveNative.init();
   runApp(
     ProviderScope(
-      child: DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) => const App(),
-      ),
+      child: DevicePreview(enabled: !kReleaseMode, builder: (context) => const App()),
     ),
   );
 }
