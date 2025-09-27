eb309b26c3df76ddfc1b436dd8be7a5dfb3c9fe6
krisnaganesha1609 <igkganes@gmail.com>
2025-09-27 13:29:38 +0700

    chore: set sdk to compatible with mobile_scanner

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.hydro_iot"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "29.0.13113456"
 
     compileOptions {
@@ -24,8 +24,8 @@ android {
         applicationId = "com.example.hydro_iot"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        minSdk = 23
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
